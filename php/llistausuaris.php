159a75fed25add21d6b71b47dcf5272c4b6d9bfb
pau <you@example.com>
2015-01-21 09:40:58 +0100

    client

--- php/llistausuaris.php
+++ php/llistausuaris.php
@@ -12,6 +12,18 @@
 	<?php
 	 $dbc = mysqli_connect('172.16.11.108', 'pau', 'pau', 'SIRIUS_DB')
         or die('Error connecting to MySQL server.');
+        
+        if (isset($_POST['submit'])) {
+			echo "esborrar usuaris<br/>";
+			foreach ($_POST['todelete'] as $delete_id){
+				echo "...esborrant $delete_id<br/>";
+				$sql="DELETE FROM users where id=$delete_id";
+				$result = mysqli_query($dbc, $sql) or die('Error querying database.');
+       }
+       
+   }
+			
+			
      $sql = "select id,Usuari,Nom,Llinatge,Llinatge2 from users;";
 	 $result = mysqli_query($dbc, $sql) or die('Error querying database.');
 
@@ -24,11 +36,14 @@
 				<th>Llinatge</th>
 				<th>Llinatge2</th>
 			</tr>
+			
+	 <form method="post" action="llistatusuaris.php"> 
+
 	<?php
 	while($row = mysqli_fetch_array($result)){
 		?>
 		<tr>
-			<td>&nbsp;</td>
+			<td><input type="checkbox" value="<?= $row['id']?>" name="todelete[]" /></td>			
 			<td><a href="updateuser.php?id=<?= $row['id']?>"><?=$row['Usuari']?></a></td>
 			<td><?=$row['Nom']?></td>
 			<td><?=$row['Llinatge']?></td>
@@ -39,6 +54,8 @@
 		}
 	?>
 	</table>
-	
+	 <input type="submit" name="submit" value="esborrar seleccionats" />
+    </form>
+
 </body>
 </html>
